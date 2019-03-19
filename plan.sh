afd3941cc53d1ddeda4e222c066baa83ee106f54
Sean Beard <sean.beard@dimensiondata.com>
2019-03-20 09:23:51 +1300

    initial commit

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 scaffolding_policy_name='webserver'
-pkg_name=apache2
-pkg_origin=seankbeard
+pkg_name='apache2'
+pkg_origin='seankbeard'
 pkg_version='0.1.0'
 pkg_upstream_url="http://chef.io"
 pkg_scaffolding="core/scaffolding-chef"
