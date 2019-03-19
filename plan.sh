0b356e9b3dc4c4fd652d0781857fdb594f3bb456
Sean Beard <sean.beard@dimensiondata.com>
2019-03-20 09:43:45 +1300

    initial commit

--- plan.sh
+++ plan.sh
@@ -1,7 +1,16 @@
-scaffolding_policy_name='webserver'
-pkg_name='apache2'
-pkg_origin='seankbeard'
-pkg_version='0.1.0'
-pkg_upstream_url='http://chef.io'
-pkg_scaffolding='core/scaffolding-chef'
-pkg_svc_user=('root')
+if [ -z ${CHEF_POLICYFILE+x} ]; then
+  echo "You must set CHEF_POLICYFILE to a policyfile name."
+  echo
+  echo "For example: env CHEF_POLICYFILE=base build"
+  exit 1
+fi
+
+CHEF_POLICYFILE=webserver
+scaffold_policy_name="$CHEF_POLICYFILE"
+pkg_name=apache2
+pkg_origin=seankbeard
+pkg_version="0.1.0"
+pkg_description="The Chef $scaffold_policy_name Policy"
+pkg_upstream_url="http://chef.io"
+pkg_scaffolding="core/scaffolding-chef"
+pkg_svc_user=("root")
