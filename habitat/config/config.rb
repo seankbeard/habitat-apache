e5f5efe103efc0735a68ec830db5ddf10bddc0b1
Sean Beard <sean.beard@dimensiondata.com>
2019-03-20 13:15:17 +1300

    adding ver

--- habitat/config/config.rb
+++ habitat/config/config.rb
@@ -3,8 +3,8 @@ node_path "$pkg_svc_data_path/nodes"
 ssl_verify_mode :verify_none
 chef_zero.enabled true
 
-interval = 1800
-splay = 180
+interval = 300
+#splay = 180
 log_level = "warn"
 env_path_prefix = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
 
