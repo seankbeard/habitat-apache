9014b9c65736bf26723f48b907e5126644a185dc
Sean Beard <sean.beard@dimensiondata.com>
2019-03-27 12:13:16 +1300

    adding audit

--- policyfiles/webserver.rb
+++ policyfiles/webserver.rb
@@ -11,8 +11,9 @@ default_source :supermarket
 
 # Specify a custom source for a cookbook:
 cookbook 'apache', github: 'seankbeard/cookbooks', branch: 'master', rel: 'apache'
+cookbook 'audit_agr', git: 'https://github.com/anthonygrees/audit_agr.git'
 
 # # run_list: chef-client will run these recipes in the order specified.
 run_list ['apache::default',
-          'audit',
+          'audit_agr',
           'os-hardening']
